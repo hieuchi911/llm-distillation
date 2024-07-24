6351f2bde6c27878f91d2994c38a685f0106e9bb
Hieu Nguyen <hieuchi911@gmail.com>
2024-07-24 11:18:27 -0700

    Add script for augmenting from train and val set jsons

--- generate.sh
+++ generate.sh
@@ -1,21 +1,36 @@
 export PROJECT_DIR="cs566"
 
 MODEL_ID="meta-llama/Meta-Llama-3-70B-Instruct"
-DATASET_ID="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/valid.json"
-SAVE_PATH="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/valid-seqkd.json"
+TRAIN_DATASET_ID="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/train.json"
+TRAIN_SAVE_PATH="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/train-seqkd.json"
+VAL_DATASET_ID="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/valid.json"
+VAL_SAVE_PATH="/home/zihaoh/repos/i-am-sober/processed_data/cnn_dailymail/full-1024-512/llama/valid-seqkd.json"
 MAPPING="benchmark/mapping/CnndmSum-llamafactory.json"
 TASK="summary_news"
+GPUS=4
 
-echo "python datasets/generator.py --model_id ${MODEL_ID} --dataset_id ${DATASET_ID} --save_path ${SAVE_PATH} --number_few_shot 2 --bfloat --mapping ${MAPPING} --batch_size 16 --task ${TASK} --gpus 2 --from_disk"
-
+echo "python datasets/generator.py --model_id ${MODEL_ID} --dataset_id ${VAL_DATASET_ID} --save_path ${VAL_SAVE_PATH} --number_few_shot 2 --bfloat --mapping ${MAPPING} --batch_size 16 --task ${TASK} --gpus 2 --from_disk"
 python datasets/generator.py \
 --model_id ${MODEL_ID} \
---dataset_id ${DATASET_ID} \
---save_path ${SAVE_PATH} \
+--dataset_id ${VAL_DATASET_ID} \
+--save_path ${VAL_SAVE_PATH} \
 --number_few_shot 2 \
 --bfloat \
 --mapping ${MAPPING} \
 --batch_size 16 \
 --task ${TASK} \
---gpus 2 \
+--gpus ${GPUS} \
+--from_disk
+
+echo "python datasets/generator.py --model_id ${MODEL_ID} --dataset_id ${TRAIN_DATASET_ID} --save_path ${TRAIN_SAVE_PATH} --number_few_shot 2 --bfloat --mapping ${MAPPING} --batch_size 16 --task ${TASK} --gpus 2 --from_disk"
+python datasets/generator.py \
+--model_id ${MODEL_ID} \
+--dataset_id ${TRAIN_DATASET_ID} \
+--save_path ${TRAIN_SAVE_PATH} \
+--number_few_shot 2 \
+--bfloat \
+--mapping ${MAPPING} \
+--batch_size 16 \
+--task ${TASK} \
+--gpus ${GPUS} \
 --from_disk
